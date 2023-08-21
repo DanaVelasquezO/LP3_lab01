313d75c91c6531ec40aa393e8e337b118dd4492d
Dana <danacarol.velasquez@gmail.com>
2023-08-20 20:43:46 -0500

    Principal3

--- Principal.java
+++ Principal.java
@@ -8,25 +8,29 @@ public class Principal {
         Scanner sc = new Scanner(System.in);
         int intentos = 0;
 
+        System.out.println("Bienvenido al juego de adivinanza de números.");
+        System.out.println("Tienes " + INTENTOS_MAXIMOS + " intentos para adivinar el número secreto.");
+
         while (intentos < INTENTOS_MAXIMOS) {
             System.out.print("Intento " + (intentos + 1) + "/" + INTENTOS_MAXIMOS +
-                    ": ¿Cual es el número secreto? ");
+                    ": ¿Cuál es el número secreto? ");
             int numeroIngresado = sc.nextInt();
 
             if (numeroIngresado == NUMERO_SECRETO) {
                 System.out.println("¡Adivinaste! ¡Eres un ganador!");
                 break;
             } else {
-                System.out.println("No es el numero correcto. ¡Intentalo de nuevo!");
+                System.out.println("No es el número correcto. ¡Inténtalo de nuevo!");
             }
 
             intentos++;
         }
 
         if (intentos == INTENTOS_MAXIMOS) {
-            System.out.println("¡Ups, has agotado tus intentos! El numero secreto era " + NUMERO_SECRETO);
+            System.out.println("¡Ups, has agotado tus intentos! El número secreto era " + NUMERO_SECRETO);
         }
 
+        System.out.println("Gracias por jugar.");
         sc.close();
     }
 }
