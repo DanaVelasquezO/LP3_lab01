6bebe8aa5cb47078371e422568d6041a813bb88d
Dana <danacarol.velasquez@gmail.com>
2023-08-20 20:42:12 -0500

    Principal2

--- Principal.java
+++ Principal.java
@@ -2,22 +2,31 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        int vida, rp;
-        boolean fl = false;
-        vida = 0;
+        final int NUMERO_SECRETO = 1234;
+        final int INTENTOS_MAXIMOS = 3;
+
         Scanner sc = new Scanner(System.in);
+        int intentos = 0;
 
-        do {
-            vida++;
-            System.out.print("Vida:" + vida + "\t¿Cuál es el número secreto? ");
-            rp = sc.nextInt();
-            if (rp == 1234)
-                fl = true;
-        } while (vida < 3 && !fl);
+        while (intentos < INTENTOS_MAXIMOS) {
+            System.out.print("Intento " + (intentos + 1) + "/" + INTENTOS_MAXIMOS +
+                    ": ¿Cual es el número secreto? ");
+            int numeroIngresado = sc.nextInt();
 
-        if (fl == true)
-            System.out.println("¡Adivinaste!");
-        else
-            System.out.println("¡Ups, perdiste...");
+            if (numeroIngresado == NUMERO_SECRETO) {
+                System.out.println("¡Adivinaste! ¡Eres un ganador!");
+                break;
+            } else {
+                System.out.println("No es el numero correcto. ¡Intentalo de nuevo!");
+            }
+
+            intentos++;
+        }
+
+        if (intentos == INTENTOS_MAXIMOS) {
+            System.out.println("¡Ups, has agotado tus intentos! El numero secreto era " + NUMERO_SECRETO);
+        }
+
+        sc.close();
     }
 }
